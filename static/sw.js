e7473baaf466b51ae2c97c0c49d42004ace54bd3
Stephen Ajulu <alunje73@gmail.com>
2023-01-26 11:04:28 +0300

    Create sw.js

---
+++ static/sw.js
@@ -0,0 +1,38 @@
+// Import Workbox Script
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+
+// Import Workbox
+import { registerRoute, setCatchHandler } from 'workbox-routing';
+import { precacheAndRoute } from 'workbox-precaching';
+import { NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies';
+
+// precache all files
+precacheAndRoute(self.__WB_MANIFEST);
+
+// Register a route for all other requests
+registerRoute(
+  ({ request }) => request.destination === 'document',
+  new NetworkFirst({
+    cacheName: 'html-cache',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 7,
+        maxEntries: 20
+      })
+    ]
+  })
+);
+
+// Register a route for all other assets
+registerRoute(
+  ({ request }) => request.destination !== 'document',
+  new StaleWhileRevalidate({
+    cacheName: 'assets-cache',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 14,
+        maxEntries: 50
+      })
+    ]
+  })
+);
